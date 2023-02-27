3828b048b4fd1ef0b6bb3c653bd4d3a53365bd7d
lucadard <lucadardenne@hotmail.com>
2023-02-27 11:12:22 -0300

    modify next root files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,48 @@
 import '@/styles/globals.css'
+import '@/styles/noise.css'
+
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import React, { useEffect, useState } from 'react'
+import { isMobile } from 'react-device-detect'
+import { ThemeProvider } from 'next-themes'
+import { AnimatePresence } from 'framer-motion'
+
+import { MouseProvider } from '@/context/MouseContext'
+import { SlideDirectionProvider } from '@/context/SlideDirectionContext'
+import Cursor from '@/components/Cursor'
+import Noise from '@/components/Noise'
+import PageLoader from './PageLoader'
+
+const loaderDuration = 1.5 // in seconds
+
+export default function App({ Component, pageProps, router }: AppProps) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMounted(true)
+    }, loaderDuration * 1000)
+  }, [])
+
+  return (
+    <ThemeProvider storageKey="theme" enableSystem={true}>
+      <MouseProvider>
+        <SlideDirectionProvider>
+          {!isMobile && <Cursor />}
+          {/* <Noise /> */}
+          <AnimatePresence mode="wait">
+            {mounted ? (
+              <Component {...pageProps} key={router.asPath + '-content'} />
+            ) : (
+              <PageLoader
+                duration={loaderDuration}
+                key={router.asPath + '-loader'}
+              />
+            )}
+          </AnimatePresence>
+        </SlideDirectionProvider>
+      </MouseProvider>
+    </ThemeProvider>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,25 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head />
+      <link rel="icon" href="/favicon.svg" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;700&family=Roboto+Mono:wght@300;400;700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,600;1,100;1,400;1,600&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+      />
       <body>
         <Main />
         <NextScript />

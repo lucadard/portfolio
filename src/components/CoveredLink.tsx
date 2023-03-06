c56697aa065d98032d96ce3f0c54aa4b42d3350f
lucadard <lucadardenne@hotmail.com>
2023-03-06 16:59:46 -0300

    change component to work without href prop

--- src/components/CoveredLink.tsx
+++ src/components/CoveredLink.tsx
@@ -5,36 +5,45 @@ import React from 'react'
 
 type Props = {
   text: string
-  href: string
+  href?: string | null
   blank?: boolean
 }
 
-const CoveredLink = ({ href, text, blank = true }: Props) => {
+const CoveredLink = ({ href = undefined, text, blank = true }: Props) => {
   const { setMouseHoverState, restoreMouseState } = useMouse()
-  return (
+
+  const content = () => (
+    <motion.span
+      initial="hide"
+      animate="show"
+      whileHover="hover"
+      className="relative inline-block group"
+    >
+      <span className="z-10 group-hover:text-white transition-colors duration-200">
+        {text}
+      </span>
+      <motion.span
+        variants={{
+          hide: { opacity: 0, height: 0 },
+          show: { opacity: 1, height: 2 },
+          hover: { opacity: 1, height: '100%' }
+        }}
+        transition={{ type: 'spring', mass: 0.2, stiffness: 100 }}
+        className="absolute bottom-0 left-0 w-full bg-black dark:bg-white mix-blend-exclusion"
+      />
+    </motion.span>
+  )
+  return href ? (
     <Link
       href={href}
       target={blank ? '_blank' : ''}
-      className="group"
       onMouseEnter={setMouseHoverState}
       onMouseLeave={restoreMouseState}
     >
-      <span className="relative inline-block">
-        <span className="z-10 transition-colors duration-100 dark:transition-none group-hover:text-white">
-          {text}
-        </span>
-        <motion.span
-          variants={{
-            hide: { opacity: 0 },
-            show: {
-              opacity: 1,
-              transition: { delay: 1.5 }
-            }
-          }}
-          className="absolute bottom-0 left-0 w-full h-[2px] transition-all group-hover:h-full bg-black dark:bg-white mix-blend-exclusion"
-        />
-      </span>
+      {content()}
     </Link>
+  ) : (
+    content()
   )
 }
 
